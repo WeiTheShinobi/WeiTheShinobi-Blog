58c2ebb1436f973598908ecce0ff389979e71ba4
KaoWei <qse3366909@gmail.com>
2021-03-10 02:48:34 +0800

    Log搭建

--- src/main/java/com/weitheshinobi/blog/controller/IndexController.java
+++ src/main/java/com/weitheshinobi/blog/controller/IndexController.java
@@ -2,12 +2,13 @@ package com.weitheshinobi.blog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/{id}")
+    public String index(@PathVariable Integer id){
         return "index";
     }
 }
