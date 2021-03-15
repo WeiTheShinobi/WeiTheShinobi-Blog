0ee29cf9bd3d70e9a71ab30a265005318ba298fb
KaoWei <qse3366909@gmail.com>
2021-03-15 17:38:01 +0800

    Type功能實現：完成編輯與刪除

--- src/main/java/com/weitheshinobi/blog/constant/TypeConstant.java
+++ src/main/java/com/weitheshinobi/blog/constant/TypeConstant.java
@@ -3,6 +3,7 @@ package com.weitheshinobi.blog.constant;
 public class TypeConstant {
     public static final String PAGE = "page";
     public static final String MESSAGE = "message";
+    public static final String TYPE = "type";
     public static final String RESULT_MESSAGE_SUCCESS = "操作成功";
     public static final String RESULT_MESSAGE_FAIL = "操作失敗";
     public static final String RESULT_MESSAGE_DUPLICATE = "操作失敗，名稱不能重複";
--- src/main/java/com/weitheshinobi/blog/controller/admin/TypeController.java
+++ src/main/java/com/weitheshinobi/blog/controller/admin/TypeController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -34,6 +35,12 @@ public class TypeController {
         return "admin/types-input";
     }
 
+    @RequestMapping("/admin/types/{id}/input")
+    public String editInput(@PathVariable Long id , Model model){
+        model.addAttribute(TypeConstant.TYPE,typeService.getTypeById(id));
+        return "admin/types-input";
+    }
+
 //    新增Type
     @PostMapping("/admin/types/commit")
     public String post(Type type, RedirectAttributes attributes){
@@ -50,6 +57,32 @@ public class TypeController {
         }else {
             attributes.addFlashAttribute(TypeConstant.MESSAGE,TypeConstant.RESULT_MESSAGE_FAIL);
         }
+        return "redirect:/admin/types";
+    }
+
+//    修改Type
+    @PostMapping("/admin/types/{id}")
+    public String editPost(Type type,@PathVariable Long id, RedirectAttributes attributes){
+        Type typeByName = typeService.getTypeByName(type.getName());
+        if(typeByName != null){
+            attributes.addFlashAttribute(TypeConstant.MESSAGE,TypeConstant.RESULT_MESSAGE_DUPLICATE);
+            return "redirect:/admin/types";
+        }
+
+        Type type1 = typeService.updateType(id,type);
+//        成功失敗的回饋訊息
+        if(type1 != null){
+            attributes.addFlashAttribute(TypeConstant.MESSAGE,TypeConstant.RESULT_MESSAGE_SUCCESS);
+        }else {
+            attributes.addFlashAttribute(TypeConstant.MESSAGE,TypeConstant.RESULT_MESSAGE_FAIL);
+        }
+        return "redirect:/admin/types";
+    }
+
+    @RequestMapping("/admin/types/{id}/delete")
+    public String delete(@PathVariable Long id,RedirectAttributes attributes){
+        typeService.deleteType(id);
+        attributes.addFlashAttribute(TypeConstant.MESSAGE,TypeConstant.RESULT_MESSAGE_SUCCESS);
         return "redirect:/admin/types";
     }
 
